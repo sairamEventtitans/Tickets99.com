1f7652834a9692be9d811b5e15ccb6b343cd2212
prabhas <sharathchandra@eventtitans.com>
2024-02-29 19:02:49 +0530

    Update

--- src/main/java/com/utils/Utils.java
+++ src/main/java/com/utils/Utils.java
@@ -102,15 +102,12 @@ public class Utils extends TestBase {
 		js.executeScript("arguments[0].scrollIntoView(true);", webelement);
 
 	}
-	
-	
+
 	public static void scrollDown() {
-		
+
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("windows.scrollBy(0,150)");
-		
-		
-		
+
 	}
 
 	public static void CapturescreenShot(String methodname) throws Throwable {
@@ -124,79 +121,72 @@ public class Utils extends TestBase {
 		FileUtils.copyFile(src, tgt);
 
 	}
-	
-	public static void writeInToExcel(String ORDERID) {
-		
 
-        // Create a workbook
-        try (Workbook workbook = new XSSFWorkbook()) {
-            // Create a sheet
-            Sheet sheet = workbook.createSheet("Order IDs");
+	public static void writeInToNewExcel(String ORDERID) {
 
-            // Create a row
-            Row row = sheet.createRow(0);
+		// Create a workbook
+		try (Workbook workbook = new XSSFWorkbook()) {
+			// Create a sheet
+			Sheet sheet = workbook.createSheet("Order IDs");
 
-            // Create a cell and set its value
-            Cell cell = row.createCell(0);
-            cell.setCellValue("Order ID");
+			// Create a row
+			Row row = sheet.createRow(0);
 
-            // Create another row for data
-            Row dataRow = sheet.createRow(1);
+			// Create a cell and set its value
+			Cell cell = row.createCell(0);
+			cell.setCellValue("Order ID");
 
-            // Create a cell for data and set its value
-            Cell dataCell = dataRow.createCell(0);
-            dataCell.setCellValue(ORDERID);
+			// Create another row for data
+			Row dataRow = sheet.createRow(1);
 
-            // Write the workbook to a file
-            try (FileOutputStream outputStream = new FileOutputStream("./src/main/java/com/testdata/demo.xlsx")) {
-                workbook.write(outputStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-		
-		public static void WriteInExistingExcel(String ORDERID,String sheetName,int cellNum) {
-			
-			 String filePath = "./src/main/java/com/testdata/TestData.xlsx";
-		        // Sheet name
-		     //   String sheetName = "Sheet1";
-		        // Order ID obtained from Selenium
-		       // String orderId = "Your_Order_ID_Goes_Here";
-		        
-		        try {
-		            FileInputStream fileInputStream = new FileInputStream(new File(filePath));
-		            Workbook workbook = new XSSFWorkbook(fileInputStream);
-		            Sheet sheet = workbook.getSheet(sheetName);
-		            
-		            // Find the next empty row in the sheet
-		           // int rowCount = sheet.getLastRowNum() + 1;
-		            Row row = sheet.getRow(1);
-		            
-		            // Write the order ID to the given cell of the row
-		            Cell cell = row.createCell(cellNum);
-		            cell.setCellValue(ORDERID);
-		            
-		            // Write the updated workbook back to the file
-		            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-		            workbook.write(fileOutputStream);
-		            fileOutputStream.close();
-		            workbook.close();
-		            
-		            System.out.println("Order ID added successfully to the Excel file.");
-		        } catch (FileNotFoundException e) {
-		            e.printStackTrace();
-		        } catch (IOException e) {
-		            e.printStackTrace();
-		        }
-		    }	
-			
-			
-			
+			// Create a cell for data and set its value
+			Cell dataCell = dataRow.createCell(0);
+			dataCell.setCellValue(ORDERID);
+
+			// Write the workbook to a file
+			try (FileOutputStream outputStream = new FileOutputStream("./src/main/java/com/testdata/demo.xlsx")) {
+				workbook.write(outputStream);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		
-		
-	
+	}
+
+	public static void WriteInExistingExcel(String ORDERID, String sheetName, int cellNum) {
+
+		String filePath = "./src/main/java/com/testdata/TestData.xlsx";
+		// Sheet name
+		// String sheetName = "Sheet1";
+		// Order ID obtained from Selenium
+		// String orderId = "Your_Order_ID_Goes_Here";
+
+		try {
+			FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+			Workbook workbook = new XSSFWorkbook(fileInputStream);
+			Sheet sheet = workbook.getSheet(sheetName);
+
+			// Find the next empty row in the sheet
+			// int rowCount = sheet.getLastRowNum() + 1;
+			Row row = sheet.getRow(1);
+
+			// Write the order ID to the given cell of the row
+			Cell cell = row.createCell(cellNum);
+			cell.setCellValue(ORDERID);
+
+			// Write the updated workbook back to the file
+			FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+			workbook.write(fileOutputStream);
+			fileOutputStream.close();
+			workbook.close();
+
+			System.out.println("Order ID added successfully to the Excel file.");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
